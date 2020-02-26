3739e72d29c3abeb64070e4284bce93f04b63b6d
Vu_Quang_Minh <vu_quang_minh@mediado.jp>
2020-02-26 19:08:41 +0900

    first commit

--- src/Main.java
+++ src/Main.java
@@ -10,27 +10,28 @@ public class Main {
         int waitingGuest = N;
         int currentWeightInElevator = 0;
         int currentGuestInElevator = 0;
-        while(waitingGuest != 0) {
+        while (waitingGuest != 0) {
             currentWeightInElevator += A[indexForTraversalArray];
             currentGuestInElevator++;
-            if(currentGuestInElevator > X || currentWeightInElevator > Y) {
+            if (currentGuestInElevator > X || currentWeightInElevator > Y) {
                 result += setOfFloor.size() + 1;
                 setOfFloor.clear();
                 currentGuestInElevator = 0;
                 currentWeightInElevator = 0;
-            }else {
+            } else {
                 waitingGuest--;
                 setOfFloor.add(B[indexForTraversalArray]);
                 indexForTraversalArray++;
             }
-            if(waitingGuest == 0) {
+            if (waitingGuest == 0) {
                 result += setOfFloor.size() + 1;
             }
         }
         return result;
     }
+
     public static void main(String[] args) {
         ResourcesScanner resourcesScanner = new ResourcesScanner("resources.txt");
-        System.out.println(solution(resourcesScanner.getA(), resourcesScanner.getB(), resourcesScanner.getN(), resourcesScanner.getM(), resourcesScanner.getX(),resourcesScanner.getY()));
+        System.out.println(solution(resourcesScanner.getA(), resourcesScanner.getB(), resourcesScanner.getN(), resourcesScanner.getM(), resourcesScanner.getX(), resourcesScanner.getY()));
     }
 }
